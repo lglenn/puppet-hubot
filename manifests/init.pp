4a7e80e2fdf28cf116c9a93f756bb78a0c569176
Larry Glenn <yrral.glenn@gmail.com>
2013-08-27 11:48:27 -0400

    Add basics

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,15 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Install Hubot
+class hubot {
+  require nodejs
+
+  $node_version = 'v0.10.13'
+
+  nodejs::module { 'hubot':
+    node_version => $node_version,
+  }
+
+  nodejs::module { 'coffeescript':
+    node_version => $node_version,
+  }
+
 }
---
+++ spec/classes/hubot_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'hubot' do
+  before do
+    @node_version = 'v0.10.13'
+  end
+  it do
+    should contain_nodejs__module('hubot').with_node_version(@node_version)
+    should contain_nodejs__module('coffeescript').with_node_version(@node_version)
+  end
+end
